157d0f23d205f103c4dbd8fa3aa6335c4de72427
SyafiqahAzizam <61213107+SyafiqahAzizam@users.noreply.github.com>
2021-03-11 07:20:30 +0800

    Add files via upload

---
+++ AnimalClassification.java
@@ -0,0 +1,198 @@
+package ai.certifai.exerciseday13;
+
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.collection.CollectionRecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.writable.Writable;
+import org.datavec.local.transforms.LocalTransformExecutor;
+import org.deeplearning4j.core.storage.StatsStorage;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.model.stats.StatsListener;
+import org.deeplearning4j.ui.model.storage.InMemoryStatsStorage;
+import org.nd4j.common.io.ClassPathResource;
+import org.nd4j.evaluation.classification.Evaluation;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
+import org.nd4j.linalg.dataset.ViewIterator;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.KFoldIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.impl.LossMCXENT;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AnimalClassification {
+
+    static final double ratio = 0.8;
+    static final int seed = 1234;
+    static  final double lr = 0.2;
+    static final int numOutputs= 7;
+    static final int numInputs= 102;
+    static final int epoch= 10;
+    static INDArray weightsArray = Nd4j.create(new double[]{0.4, 0.5, 1.0, 0.5, 0.5, 0.4,0.4});
+
+    public static void main(String[] args) throws  Exception {
+
+        //loadfile
+        File myfile =  new ClassPathResource("zoo.csv").getFile();
+
+        //FileSplit
+        FileSplit fileSplit = new FileSplit(myfile);
+
+        //set CSV Record Reader and initialize it
+        RecordReader rr = new CSVRecordReader(1,',');
+        rr.initialize(fileSplit);
+
+        //Build Schema
+        Schema schema = new Schema.Builder()
+                .addColumnsString("Animal_names")
+                .addColumnCategorical("hair", Arrays.asList("0","1"))
+                .addColumnCategorical("feathers", Arrays.asList("0", "1"))
+                .addColumnCategorical("eggs", Arrays.asList("0", "1"))
+                .addColumnCategorical("milk", Arrays.asList("0", "1"))
+                .addColumnCategorical("airbone", Arrays.asList("0","1"))
+                .addColumnCategorical("aquatic", Arrays.asList("0","1"))
+                .addColumnCategorical("predator", Arrays.asList("0","1"))
+                .addColumnCategorical("toothed", Arrays.asList("0","1"))
+                .addColumnCategorical("backbone", Arrays.asList("0","1"))
+                .addColumnCategorical("breathes", Arrays.asList("0","1"))
+                .addColumnCategorical("venomous", Arrays.asList("0","1"))
+                .addColumnCategorical("tail", Arrays.asList("0","1"))
+                .addColumnCategorical("domestic", Arrays.asList("0","1"))
+                .addColumnCategorical("catsize", Arrays.asList("0","1"))
+                .addColumnCategorical("class_type", Arrays.asList("0","1","2","3","4","5","6","7"))
+                .addColumnInteger("legs")
+                .build();
+
+        //Build TransformProcess to transform the data
+        TransformProcess transform = new TransformProcess.Builder(schema)
+                .build();
+
+        // Checking the schema
+        Schema outputSchema = transform.getFinalSchema();
+        System.out.println(outputSchema);
+
+        List<List<Writable>> allData = new ArrayList<>();
+
+        while(rr.hasNext()){
+            allData.add(rr.next());
+        }
+
+        List<List<Writable>> processData = LocalTransformExecutor.execute(allData, transform);
+
+        //Create iterator from process data
+        CollectionRecordReader collectionRR = new CollectionRecordReader(processData);
+
+        //Input batch size , label index , and number of label
+        DataSetIterator dataSetIterator = new RecordReaderDataSetIterator(collectionRR, processData.size(),-1,7);
+
+        //Create Iterator and shuffle the dat
+        DataSet fullDataset = dataSetIterator.next();
+        fullDataset.shuffle(seed);
+
+//      //Input split ratio
+        SplitTestAndTrain testAndTrain = fullDataset.splitTestAndTrain(ratio);
+//
+        //Get train and test dataset
+        DataSet trainData = testAndTrain.getTrain();
+        DataSet testData = testAndTrain.getTest();
+
+        //printout size
+        System.out.println("Training vector : ");
+        System.out.println(Arrays.toString(trainData.getFeatures().shape()));
+        System.out.println("Test vector : ");
+        System.out.println(Arrays.toString(testData.getFeatures().shape()));
+
+        //Data normalization
+        DataNormalization normalizer = new NormalizerMinMaxScaler();
+        normalizer.fit(trainData);
+        normalizer.transform(trainData);
+        normalizer.transform(testData);
+
+        //Network config
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .weightInit(WeightInit.XAVIER)
+                .updater(new Nesterovs(lr, Nesterovs.DEFAULT_NESTEROV_MOMENTUM))
+                .l2(0.001)
+                .list()
+                .layer(0, new DenseLayer.Builder()
+                        .nIn(numInputs)
+                        .nOut(100)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(1, new DenseLayer.Builder()
+                        .nIn(100)
+                        .nOut(75)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(2, new DenseLayer.Builder()
+                        .nIn(75)
+                        .nOut(90)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(3, new DenseLayer.Builder()
+                        .nIn(90)
+                        .nOut(150)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(4, new OutputLayer.Builder()
+                        .nIn(40)
+                        .nOut(numOutputs)
+                        .lossFunction(new LossMCXENT(weightsArray))
+                        .activation(Activation.SOFTMAX)
+                        .build())
+                .build();
+
+
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+        //UI-Evaluator
+        StatsStorage storage = new InMemoryStatsStorage();
+        UIServer server = UIServer.getInstance();
+        server.attach(storage);
+
+        //Set model listeners
+        model.setListeners(new StatsListener(storage, 10));
+
+
+        //Training
+        Evaluation eval;
+        for(int i=0; i < epoch; i++) {
+            model.fit(trainData);
+            eval = model.evaluate(new ViewIterator(testData, processData.size()));
+            System.out.println("EPOCH: " + i + " Accuracy: " + eval.accuracy());
+        }
+
+
+        //Confusion matrix
+        Evaluation evalTrain = model.evaluate(new ViewIterator(trainData, processData.size()));
+        Evaluation evalTest = model.evaluate(new ViewIterator(testData,processData.size()));
+        System.out.print("Train Data");
+        System.out.println(evalTrain.stats());
+
+        System.out.print("Test Data");
+        System.out.print(evalTest.stats());
+    }
+}
---
+++ FishMarket.java
@@ -0,0 +1,179 @@
+package ai.certifai.exerciseday13;
+
+
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.collection.CollectionRecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.writable.Writable;
+import org.datavec.local.transforms.LocalTransformExecutor;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.common.io.ClassPathResource;
+import org.datavec.api.transform.schema.Schema;
+import org.nd4j.evaluation.classification.Evaluation;
+
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.KFoldIterator;
+
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+// a record of 7 common different fish species in fish market sales.
+// a predictive model can be performed using machine friendly data
+// estimate the weight of fish can be predicted.
+
+public class FishMarket {
+
+    static int seed = 1234;
+    static double lr = 0.01;
+    static int numInp =7;
+    static int numOut =7;
+    static int epoch = 500;
+    public static final String BLACK_BOLD = "\033[1;30m";
+    public static final String BLUE_BOLD = "\033[1;34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+
+    public static void main(String[] args) throws Exception{
+
+        //set filepath
+        File myfile = new ClassPathResource("Fish.csv").getFile();
+
+        //Split
+        FileSplit fileSplit = new FileSplit(myfile);
+
+        //set CSV Record Reader and initialize it
+        RecordReader rr = new CSVRecordReader(1,',');
+        rr.initialize(fileSplit);
+
+        //Build Schema
+        Schema schema = new Schema.Builder()
+                .addColumnCategorical("Species", Arrays.asList("Bream","Parkki","Porch","Roach","Whitefish","Smelt","Perch"))
+                .addColumnsFloat("Weight","Length1","Length2","Height","Width")
+                .build();
+
+        //Build TransformProcess to transform the data
+        TransformProcess transform = new TransformProcess.Builder(schema)
+                .categoricalToInteger("Species")
+                .build();
+
+        List<List<Writable>> data = new ArrayList<>();
+        while(rr.hasNext())
+        {
+            data.add(rr.next());
+        }
+        List<List<Writable>> processData = LocalTransformExecutor.execute(data, transform);
+
+        //Create iterator from process data
+        CollectionRecordReader collectionRR = new CollectionRecordReader(processData);
+
+        //Input batch size , label index , and number of label
+        DataSetIterator iter = new RecordReaderDataSetIterator(collectionRR, processData.size(),-1,7, true);
+
+        //Create Iterator and shuffle the dat
+        DataSet fullDataset = iter.next();
+        fullDataset.shuffle(seed);
+
+        //create kfold iterator (k=5)
+        KFoldIterator kFoldIterator = new KFoldIterator(5, fullDataset);
+
+
+        //Network config
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .weightInit(WeightInit.XAVIER)
+                .updater(new Adam(lr))
+                .l2(0.2)
+                .list()
+                .layer(new DenseLayer.Builder()
+                        .nIn(numInp)
+                        .nOut(128)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(new DenseLayer.Builder()
+                        .nIn(128)
+                        .nOut(64)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(new OutputLayer.Builder()
+                        .nIn(64)
+                        .nOut(numOut)
+                        .activation(Activation.SOFTMAX)
+                        .lossFunction(LossFunctions.LossFunction.MSE)
+                        .build())
+                .build();
+
+        //start the kfold evaluation
+        int i = 1;
+
+
+        //initialize an empty list to store the F1 score
+        ArrayList<Double> f1List = new ArrayList<>();
+
+        //for each fold
+        while (kFoldIterator.hasNext()) {
+            System.out.println(BLACK_BOLD + "Fold: " + i + ANSI_RESET);
+
+            //for each fold, get the features and labels from training set and test set
+            DataSet currDataSet = kFoldIterator.next();
+            INDArray trainFoldFeatures = currDataSet.getFeatures();
+            INDArray trainFoldLabels = currDataSet.getLabels();
+            INDArray testFoldFeatures = kFoldIterator.testFold().getFeatures();
+            INDArray testFoldLabels = kFoldIterator.testFold().getLabels();
+            DataSet trainDataSet = new DataSet(trainFoldFeatures, trainFoldLabels);
+
+            //scale the dataset
+            NormalizerMinMaxScaler scaler = new NormalizerMinMaxScaler();
+            scaler.fit(trainDataSet);
+            scaler.transform(trainFoldFeatures);
+            scaler.transform(testFoldFeatures);
+
+            //initialize the model
+            MultiLayerNetwork model = new MultiLayerNetwork(conf);
+            model.init();
+
+            //train the data
+            for (int j = 0; j < epoch; j++) {
+                model.fit(trainDataSet);
+            }
+
+            //evaluate the model with test set
+            Evaluation eval = new Evaluation();
+            eval.eval(testFoldLabels, model.output(testFoldFeatures));
+
+            //print out the evaluation results
+            System.out.println(eval.stats());
+            //save the eval results
+            f1List.add(eval.f1());
+
+            i++;
+
+        }
+
+        INDArray f1scores = Nd4j.create(f1List);
+        System.out.println("Average F1 scores for all folds: " + f1scores.mean(0));
+
+
+
+    }}
